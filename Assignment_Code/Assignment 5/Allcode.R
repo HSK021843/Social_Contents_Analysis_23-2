737541613fab7b70d7e6a670dbb7277e41f03b78
HSK021843 <135344588+HSK021843@users.noreply.github.com>
2023-09-25 16:49:09 +0900

    Update Allcode.R

--- Assignment_Code/Assignment 5/Allcode.R
+++ Assignment_Code/Assignment 5/Allcode.R
@@ -10,3 +10,31 @@ fus <- image_composite(flower, mount, offset = geometry_point(120, 30))
 print(fus)
 
 
+
+sun <- image_read("sun.jfif")
+mount < image_read("mount3.png")
+
+flower <- image_scale(sun, "50x50!")
+mount <- image_scale(mount, "600x150!")
+
+fus <- image_composite(sun, mount, offset = geometry_point(120, 30))
+print(fus)
+
+
+
+sun <- image_read("sun.jfif")
+mount < image_read("mount3.png")
+
+flower <- image_scale(sun, "50x50!")
+mount <- image_scale(mount, "600x150!")
+
+while (TRUE) {
+  posit = geometry_point(120, 30)
+  fus <- image_composite(sun, mount, offset = position)
+  print(img)
+  Sys.sleep(0, 1)
+  if(x == 400)
+    break
+  x <- x + 20
+  y <- y - 4
+}
