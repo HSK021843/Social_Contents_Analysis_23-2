0b08c404260eb59878354a42fc2fd3d68faee9ee
HSK021843 <135344588+HSK021843@users.noreply.github.com>
2023-10-02 15:32:36 +0900

    update

--- Assignment_Code/Assignment 4/#4-4.R
+++ Assignment_Code/Assignment 4/#4-4.R
@@ -10,7 +10,7 @@ df2 <- data.frame(기업 = company, 매출 = rev_2022, 연도 = "2022년")
 df <- rbind(df1, df2)
 df
 
-ggplot(df, aes(x = 연도, y = 매출, group = 기업, color = 기업)) +
+ggplot(data =df, aes(x = 연도, y = 매출, group = 기업, color = 기업)) +
   geom_line(size = 1.5) +
   geom_point(color = "red", size = 2) +
   labs(title = "기업별 연도별 매출")
--- Assignment_Code/Assignment 4/#4-6.R
+++ Assignment_Code/Assignment 4/#4-6.R
@@ -4,4 +4,4 @@ data(mtcars)
 
 ggplot(data = mtcars, aes(y = hp)) +
   geom_boxplot(col = "black", fill = "lightblue") +
-  labs(title = "hp의 분포", x = "구간", y = "hp", width = 0.5)
+  labs(title = "hp의 분포", x = "", y = "hp")
