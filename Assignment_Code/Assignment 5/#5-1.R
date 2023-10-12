31b15d9ba1f7c26bd212de851e3024d2ea688083
HSK021843 <135344588+HSK021843@users.noreply.github.com>
2023-10-12 23:48:10 +0900

    finish

--- Assignment_Code/Assignment 5/#5-1.R
+++ Assignment_Code/Assignment 5/#5-1.R
@@ -1,8 +1,11 @@
 # 5-1. 첨부된 꽃 사진 (flo4.png)과 산 사진(mount.jpg)을 확인해 보세요.
+# 두 사진의 크기를 적당히 조정한 후 산 사진 위 적당한 위치에 꽃을 합성시켜 주세요.
 library("magick")
 
 flower <- image_read("flo4.png")
 mount <- image_read("mount.jpg")
+flower
+mount
 
 flower <- image_scale(flower, "50x50!")
 mount <- image_scale(mount, "1000x525!")
--- Assignment_Code/Assignment 5/#5-3-r.R
+++ Assignment_Code/Assignment 5/#5-3-r.R
@@ -9,14 +9,13 @@ mount <- image_scale(mount, "740x400!")
 x = 120
 y = 150
 
-# 이미지 객체 목록을 초기화합니다
+
 img_list <- list()
 
 while (TRUE) {
   posit = geometry_point(x, y)
   fus <- image_composite(mount, sun, offset = posit)
   
-  # 이미지를 목록에 추가합니다
   img_list <- c(img_list, fus)
   
   if(x == 300)
@@ -26,10 +25,8 @@ while (TRUE) {
   y <- y - 15
 }
 
-# 이미지 객체 목록을 하나의 magick 이미지 객체로 결합합니다
 img <- image_join(img_list)
+img <- image_animate(img, loop = 1)
 
-# 이미지 객체를 애니메이션 GIF로 변환하고 저장합니다
-img <- image_animate(img)
 image_write(img, "result.gif")
  
